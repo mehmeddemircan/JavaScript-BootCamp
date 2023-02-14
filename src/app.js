0744c5c0b57ad9a5ca0f8f4c037c5ad8bacc815e
mehmet demircan <mehmeddemircan@gmail.com>
2023-02-14 11:37:08 +0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,16 @@
+console.log("Merhaba Kodlama.io")
+
+
+//JS type safe değildir
+let dolarDun = 9.20 
+
+console.log(dolarBugun)
+
+{
+    // burası ayrı bir dünya
+    let dolarDun = 9.10
+    console.log(dolarDun)
+}
+console.log(dolarDun)
+
+let dolarBugun = 9.30
